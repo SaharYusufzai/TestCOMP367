7f63ed688444cb6bc165716442961d9a2b3ac2c3
Sahar Yusufzai <113653311+SaharYusufzai@users.noreply.github.com>
2024-03-07 02:02:41 -0500

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Good morning, SAHAR YUSUFZAI, Welcome to COMP367", app.getMessage());
+        assertEquals("Welcome to COMP367", app.getMessage());
     }
 }
